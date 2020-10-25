e6c0b9a9dee04fc2693749116199e94ae45521b0
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2020-10-25 21:46:19 +0700

    Update lab09.c

--- lab09.c
+++ lab09.c
@@ -34,3 +34,35 @@ Jim is in the list.
 Process finished with exit code 0
 
 //Q.1.2
+#include<stdio.h>
+#include<string.h>
+#define NUMSTR 7
+#define MAXLEN 30
+
+int searchString(char list[NUMSTR][MAXLEN], char query[] ){
+    int i,count = 0;
+    char message[MAXLEN];
+    strcpy( message, query );
+    for( i=0; i<NUMSTR; i++ )
+        if( strcmp( list[i], query ) == 0 ) {
+            count++;
+        }
+    return count;
+    strcat( message, " is NOT in the list.");
+        puts( message );
+}
+void main(){
+    char nameList[NUMSTR][MAXLEN] = {"Joe", "Jane", "Joe","John","Jim", "Joe", "Jim"};
+    puts("\n----------Searching----------");
+    printf("Joe:%d\n",searchString( nameList, "Joe"));
+    printf("Jack:%d\n",searchString( nameList, "Jack"));
+    printf("Jim:%d\n",searchString( nameList, "Jim"));
+}
+
+Output:
+----------Searching----------
+Joe:3
+Jack:0
+Jim:2
+
+Process finished with exit code 0
